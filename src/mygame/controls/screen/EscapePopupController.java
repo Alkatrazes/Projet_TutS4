5c8d8532eb918581085537831ac4d3e85917e64c
Alkatrazes <raph.rousseau11@gmail.com>
2017-03-12 18:05:44 +0100

    Finalisation de la popup + création des méthodes appelantes de la popup

--- src/mygame/controls/screen/EscapePopupController.java
+++ src/mygame/controls/screen/EscapePopupController.java
@@ -4,10 +4,41 @@
  */
 package mygame.controls.screen;
 
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.screen.ScreenController;
+import mygame.Main;
+
 /**
  *
  * @author root
  */
-public class EscapePopupController {
+public class EscapePopupController implements ScreenController {
+    
+    private Nifty nifty;
+    
+    public void options() {
+        
+    }
+    
+    public void backTitle() {
+        nifty.gotoScreen("StartScreen");
+    }
+    
+    public void quit() {
+        Main.app.stop();
+    }
+
+    public void bind(Nifty nifty, Screen screen) {
+        this.nifty = nifty;
+    }
+
+    public void onStartScreen() {
+
+    }
+
+    public void onEndScreen() {
+
+    }
     
 }
