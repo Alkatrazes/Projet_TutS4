ef9f5e0fa12a1345ba6d4ec61bad226f593c0c0b
Alkatrazes <raph.rousseau11@gmail.com>
2017-03-13 13:41:07 +0100

    KeyBidings

---
+++ src/mygame/keyBiding.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame;
+
+import com.jme3.input.KeyInput;
+
+/**
+ *
+ * @author root
+ */
+public class keyBiding {
+    
+    public int HERO_FORWARD = KeyInput.KEY_UP;
+    public int HERO_BACKWARD = KeyInput.KEY_DOWN;
+    public int HERO_LEFT = KeyInput.KEY_LEFT;
+    public int HERO_RIGHT = KeyInput.KEY_RIGHT;
+    
+    public int HERO_INVENTAIRE = KeyInput.KEY_C;
+    public int HERO_SETTINGS = KeyInput.KEY_ESCAPE;
+}
