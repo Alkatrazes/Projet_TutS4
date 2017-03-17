afa58486751c7bbae6d81aab94e8016ad7a0a0ef
Alkatrazes <raph.rousseau11@gmail.com>
2017-03-17 16:25:52 +0100

    Curseur perso

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -2,8 +2,14 @@ package mygame;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.app.StatsAppState;
+import com.jme3.cursors.plugins.JmeCursor;
 import com.jme3.renderer.RenderManager;
 import com.jme3.system.AppSettings;
+import com.jme3.texture.Image;
+import com.jme3.texture.Texture;
+import com.jme3.util.BufferUtils;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
 
 import mygame.controls.screen.*;
 
@@ -31,6 +37,8 @@ public class Main extends SimpleApplication {
         setDisplayFps(false);
         setDisplayStatView(false);
         
+        UpdateCursor();
+        
         this.stateManager.attach(new GUI(this));
     }
 
@@ -42,5 +50,47 @@ public class Main extends SimpleApplication {
     @Override
     public void simpleRender(RenderManager rm) {
         //TODO: add render code
+    }
+    
+    private void UpdateCursor() {
+        Texture tex = assetManager.loadTexture("Interface/Nifty/images/curseur.png");
+        inputManager.setMouseCursor(textureToCursor(tex));
+    }
+
+    private JmeCursor textureToCursor(Texture tex) {
+        Image image = tex.getImage();
+        ByteBuffer imgByteBuff = (ByteBuffer) image.getData(0).rewind();
+        IntBuffer curIntBuff = BufferUtils.createIntBuffer(40 * 40);
+
+        for (int y = 0; y < 32; y++) //image.getHeight()
+        {
+            if (y >= image.getHeight())
+            {
+                for (int i = 0; i < image.getWidth(); i++)
+                {
+                    curIntBuff.put(0xff000000);
+                }
+                continue;
+            }
+
+            for (int x = 0; x < image.getWidth(); x++)
+            {
+                int rgba = imgByteBuff.getInt();
+                if (x >= 48) continue;
+
+                 int argb = ((rgba & 255) << 24) | (rgba >> 8);
+                 curIntBuff.put(argb);
+            }
+        }
+
+        JmeCursor c = new JmeCursor();
+        c.setHeight(image.getHeight()); //image.getHeight()-2);
+        c.setWidth(32); //image.getWidth());
+        c.setNumImages(1);
+        c.setyHotSpot(27 - 3); //image.getHeight()-3);
+        c.setxHotSpot(3);
+        c.setImagesData((IntBuffer) curIntBuff.rewind());
+
+        return c;
     }
 }
