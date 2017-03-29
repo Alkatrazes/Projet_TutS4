5e406e5a0d9032a59fecf0cba83be1d8f0413551
dfjk21 <valentin.fadloun@yahoo.fr>
2017-03-30 00:13:05 +0200

    chang combat

--- src/mygame/AppStates/CombatAppState.java
+++ src/mygame/AppStates/CombatAppState.java
@@ -15,10 +15,12 @@ import com.jme3.bullet.BulletAppState;
 import com.jme3.bullet.collision.PhysicsCollisionEvent;
 import com.jme3.bullet.collision.PhysicsCollisionListener;
 import com.jme3.font.BitmapFont;
+import com.jme3.font.BitmapText;
 import com.jme3.input.FlyByCamera;
 import com.jme3.input.InputManager;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
+import com.jme3.material.Material;
 import com.jme3.math.FastMath;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
@@ -26,12 +28,18 @@ import com.jme3.renderer.Camera;
 import com.jme3.scene.CameraNode;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
+import com.jme3.terrain.geomipmap.TerrainLodControl;
+import com.jme3.terrain.geomipmap.TerrainQuad;
+import com.jme3.terrain.heightmap.AbstractHeightMap;
+import com.jme3.terrain.heightmap.ImageBasedHeightMap;
+import com.jme3.texture.Texture;
 import de.lessvoid.nifty.Nifty;
 import de.lessvoid.nifty.elements.Element;
 import de.lessvoid.nifty.elements.render.TextRenderer;
 import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.nifty.screen.ScreenController;
 import mygame.keyBiding;
+
 
 /**
  *
@@ -45,6 +53,8 @@ public class CombatAppState extends AbstractAppState implements PhysicsCollision
     private AssetManager assetManager;
     private InputManager inputManager;
     private BulletAppState bulletAppState;
+    
+    private TerrainQuad terrain;
     // private GamePlayAppState gamePlayAppState;
     // private StartScreenAppState startScreenAppState;
     private AppStateManager stateManager;
@@ -88,7 +98,69 @@ public class CombatAppState extends AbstractAppState implements PhysicsCollision
         this.stateManager = stateManager;
         this.listener = this.app.getListener();
         keyBindings = new keyBiding();
-        
+        setDisplayFps(false);
+        setDisplayStatView(false);
+        flyCam.setMoveSpeed(100);
+
+        /** 1. Create terrain material and load four textures into it. */
+        mat_terrain = new Material(assetManager,"Common/MatDefs/Terrain/Terrain.j3md");
+
+        /** 1.1) Add ALPHA map (for red-blue-green coded splat textures) */
+        mat_terrain.setTexture("Alpha", assetManager.loadTexture("Textures/Terrain/splat/alphamap.png"));
+
+        /** 1.2) Add GRASS texture into the red layer (Tex1). */
+        Texture grass = assetManager.loadTexture("Textures/Terrain/Rocky/RockyTexture.jpg");
+        grass.setWrap(Texture.WrapMode.Repeat);
+        mat_terrain.setTexture("Tex1", grass);
+        mat_terrain.setFloat("Tex1Scale", 64f);
+
+
+        /** 1.4) Add ROAD texture into the blue layer (Tex3) */
+        Texture rock = assetManager.loadTexture(
+                "Textures/Terrain/splat/road.jpg");
+        rock.setWrap(Texture.WrapMode.Repeat);
+        mat_terrain.setTexture("Tex3", rock);
+        mat_terrain.setFloat("Tex3Scale", 128f);
+
+        /** 2. Create the height map */
+        AbstractHeightMap heightmap = null;
+        Texture heightMapImage = assetManager.loadTexture("Textures/Terrain/splat/alpha1.png");
+        heightmap = new ImageBasedHeightMap(heightMapImage.getImage());
+        heightmap.load();
+
+        guiNode.detachAllChildren();
+        guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
+        BitmapText helloText = new BitmapText(guiFont, false);
+        helloText.setSize(guiFont.getCharSet().getRenderedSize());
+        helloText.setText("100/100");
+        helloText.setLocalTranslation(0, 1000, 0);
+        guiNode.attachChild(helloText);
+
+        /** 3. We have prepared material and heightmap. 
+         * Now we create the actual terrain:
+         * 3.1) Create a TerrainQuad and name it "my terrain".
+         * 3.2) A good value for terrain tiles is 64x64 -- so we supply 64+1=65.
+         * 3.3) We prepared a heightmap of size 512x512 -- so we supply 512+1=513.
+         * 3.4) As LOD step scale we supply Vector3f(1,1,1).
+         * 3.5) We supply the prepared heightmap itself.
+         */
+        int patchSize = 65;
+        terrain = new TerrainQuad("my terrain", patchSize, 513, heightmap.getHeightMap());
+
+        /** 4. We give the terrain its material, position & scale it, and attach it. */
+        terrain.setMaterial(mat_terrain);
+        terrain.setLocalTranslation(0, -100, 0);
+        terrain.setLocalScale(2f, 1f, 2f);
+        // Charge un modèle de test_data (ogrexml + matériel + texture)
+
+        rootNode.attachChild(terrain);
+
+
+        /** 5. The LOD (level of detail) depends on were the camera is: */
+        TerrainLodControl control = new TerrainLodControl(terrain, getCamera());
+        terrain.addControl(control);
+        cam.setLocation(new Vector3f(1,0,500));
+        flyCam.setEnabled(false);
         bulletAppState = stateManager.getState(BulletAppState.class);
         worldAppState = stateManager.getState(WorldAppState.class);                
         initMonster();       
